54f38eed4d8669ca4a688101e3d81d64f9c7b474
clarymt <clarymt@me.com>
2019-10-21 17:30:05 -0400

    working on constructor in word.js file

--- word.js
+++ word.js
@@ -4,7 +4,16 @@ function Word(answer) {
     this.objArray = [];
   for (var i = 0; i < answer.length; i++) {
       var letter = new Letter(answer[i]);
-      this objArray.push(letter);
+      this.objArray.push(letter);
   }
+}
+
+this.log = function () {
+    inputLog= "";
+    for (var i = 0; i < this.objArray.length; i++) {
+        inputLog += this objArray[i] + " ";
+    }
+    console.log(inputLog + "\n\n");
+}
 //= new Letter("")
 //var words = letter(i)}
